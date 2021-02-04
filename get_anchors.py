93536c68dc593a300ac8b84e93aa4b591b3867b6
michelle <phiangda@ualberta.ca>
2021-02-03 17:17:05 -0700

    code for getting anchors

---
+++ get_anchors.py
@@ -0,0 +1,31 @@
+import json
+import cv2
+import random
+import math
+from sklearn.cluster import KMeans
+import numpy as np
+
+data_path = '../BBOX-LABELS/'
+
+with open(data_path+'jsonlabels.txt') as f:
+    data = json.load(f)
+
+all_w_h = []
+for entry in data:
+    if isinstance(entry['boxes'][0], int):
+        box = entry['boxes']
+        w_h = [box[2], box[3]]
+        all_w_h.append(w_h)
+    else:
+        for box in entry['boxes']:
+            w_h = [box[2], box[3]]
+            all_w_h.append(w_h)
+
+
+sample = random.sample(all_w_h, 1000)
+sample = np.array(sample)
+kmeans = KMeans(n_clusters=10).fit(sample)
+rounded = []
+for x in kmeans.cluster_centers_:
+    rounded.append([round(x[0]), round(x[1])])
+print(rounded)
