d679c3e1488edb5496d8d7dc667a85a1cb6b921e
michelle <phiangda@ualberta.ca>
2021-02-05 11:42:38 -0700

    changed some values according to official guide

--- cfg.py
+++ cfg.py
@@ -35,8 +35,8 @@ Cfg.hue = .1
 
 Cfg.learning_rate = 0.001
 Cfg.burn_in = 1000
-Cfg.max_batches = 2000
-Cfg.steps = [1600,1800]
+Cfg.max_batches = 6000
+Cfg.steps = [4800,5400]
 Cfg.policy = Cfg.steps
 Cfg.scales = .1, .1
 
